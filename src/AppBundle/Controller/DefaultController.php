c88c911fd25a93bcc069c0703c77cec18310c818
flickerfate <flickerfate@gmail.com>
2015-08-13 01:40:53 +0300

    interim commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,22 +2,53 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\ProductType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
 
-        return $this->render('AppBundle::main.html.twig');
+        $product = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->findAll();
+
+        $form = $this->createForm(new ProductType(), new Product());
+
+        return $this->render('AppBundle::main.html.twig', array("products" => $product,"form" => $form->createView()));
 //        return $this->render('default/index.html.twig', array(
 //            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
 //        ));
     }
+
+    /**
+     * @Route("/save", name="form_target", methods="POST")
+     */
+    public function saveProductAction(Request $request, Product $product = null)
+    {
+
+        $product = (is_null($product))?(new Product()): $product;
+        $form = $this->createForm(new ProductType(),$product);
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            return new Response('Created product id '.$product->getId());
+        }
+
+        return new Response($form->getErrors());
+    }
+
+
 }
